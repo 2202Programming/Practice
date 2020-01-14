8de032fb3950606d3841873786394b2eb7549a94
Xander Ge <xander.q.ge@gmail.com>
2020-01-13 20:09:45 -0600

    Added Shaping to Joystick Inputs

--- PracticeBot/src/main/java/frc/robot/commands/ArcadeDrive.java
+++ PracticeBot/src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -3,8 +3,11 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
+import frc.robot.util.ExpoShaper;
 
 public class ArcadeDrive extends Command {
+    private ExpoShaper speedShaper;
+    private ExpoShaper rotationShaper;
 
     @Override
     protected boolean isFinished() {
@@ -16,7 +19,11 @@ public class ArcadeDrive extends Command {
     }
 
     protected void execute() {
-        Robot.driveTrain.arcadeDrive(Robot.oi.driver.getY(Hand.kLeft), Robot.oi.driver.getX(Hand.kRight));
+        // Robot.driveTrain.ArcadeDrive(0.90, 0, true);
+        double s = speedShaper.expo(Robot.oi.driver.getY(Hand.kLeft));
+        // soften the input by limiting the max input
+        double rot = rotationShaper.expo(0.8 * Robot.oi.driver.getX(Hand.kRight));
+        Robot.driveTrain.arcadeDrive(s, rot);
     }
 
 }
